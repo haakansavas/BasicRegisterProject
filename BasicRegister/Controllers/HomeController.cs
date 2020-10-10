146adc99373dc03d9abd587d9e91af7ac62b81f8
haakansavas@outlook.com <haakansavas@outlook.com>
2020-10-10 06:04:19 +0300

    Add project files.

---
+++ BasicRegister/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using BasicRegister.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicRegister.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            // Hata Yoksa Gir
+            if (ModelState.IsValid)
+            {
+                Session["user"] = model;
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BasicRegister/Models/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BasicRegister.Models
+{
+    public class RegisterViewModel
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        [Compare("Password",ErrorMessage = "Passwords are different!")]
+        public string RePassword { get; set; }
+    }
+}
